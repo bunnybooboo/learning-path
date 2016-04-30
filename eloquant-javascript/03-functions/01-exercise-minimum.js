07c610b1c1b88d57e41cb981b8064801e5dca3cf
David Ross <sw1ayfe@gmail.com>
2016-04-30 13:57:25 +0100

    finished exercise 1

---
+++ eloquant-javascript/03-functions/01-exercise-minimum.js
@@ -0,0 +1,9 @@
+// Write a function min that takes two arguments and returns their minimum
+function min(num1, num2) {
+  if(num1 < num2) {
+    return num1;
+  }
+  else {
+    return num2;
+  }
+}
---
+++ eloquant-javascript/03-functions/02-exercise-recursion.js
@@ -0,0 +1,2 @@
+/* Define a recursive function isEven. Zero is even, One is odd, for any other number (n)
+its evenness is the same as n-2. The function should accept a number parameter and return a Boolean */
