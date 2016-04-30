eb1e4d1b8f3918ebf3d928fa76bd4217fc1d4671
David Ross <sw1ayfe@gmail.com>
2016-04-30 11:49:31 +0100

    add chapter 2 exercise solutions & start ch3

---
+++ eloquant-javascript/02-program-structure/03-exercise-chessboard.js
@@ -0,0 +1,16 @@
+// chess board
+
+var size = 8;
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
