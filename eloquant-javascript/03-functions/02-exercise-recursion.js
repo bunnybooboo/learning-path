2e04a01ccb1648981888152365d2decc2da80dca
David Ross <sw1ayfe@gmail.com>
2016-04-30 14:14:35 +0100

    finished exercise 2

--- eloquant-javascript/03-functions/02-exercise-recursion.js
+++ eloquant-javascript/03-functions/02-exercise-recursion.js
@@ -1,2 +1,34 @@
 /* Define a recursive function isEven. Zero is even, One is odd, for any other number (n)
 its evenness is the same as n-2. The function should accept a number parameter and return a Boolean */
+
+/*
+my faulty attempt
+
+function isEven() {
+  if (n == 0) {
+    return true;
+  }
+  else if (n == 1) {
+    return false;
+  }
+  else {
+    if ((n-2) % 2 = 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+*/
+
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+    return false;
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n - 2);
+}
