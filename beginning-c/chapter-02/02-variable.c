9a79e942a494f180f380bb092450eab694a29d38
David Ross <sw1ayfe@gmail.com>
2016-05-01 22:19:52 +0100

    adding 02

---
+++ beginning-c/chapter-02/02-variable.c
@@ -0,0 +1,10 @@
+// first code was a repeat so headed straight to this one
+
+#include <stdio.h>
+int main(void)
+{
+  int salary;
+  salary = 10000;
+  printf("My salary is %d.\n", salary);
+  return 0;
+}
