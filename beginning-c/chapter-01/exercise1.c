a5fd1cb70231339a430dbbfabbc346e1b5543094
David Ross <sw1ayfe@gmail.com>
2016-05-01 21:46:38 +0100

    add working exercise1

--- beginning-c/chapter-01/exercise1.c
+++ beginning-c/chapter-01/exercise1.c
@@ -1,2 +1,9 @@
 // Write a program that will output your name and address using a separate
 // printf() statement for each line of output.
+#include <stdio.h>
+
+int main(void) {
+  printf("This is my name\n");
+  printf("And this is my address (I'm not that stupid!)\n");
+  return 0;
+}
