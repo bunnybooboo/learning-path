15b385b5618c91cc3338f51c625166e8d11800fb
David Ross <sw1ayfe@gmail.com>
2016-05-01 21:55:59 +0100

    finished exercise3

--- beginning-c/chapter-01/exercise3.c
+++ beginning-c/chapter-01/exercise3.c
@@ -1,2 +1,7 @@
 // Write a program to output the following text exactly as it appears here:
 // "It's freezing in here," he said coldly.
+#include <stdio.h>
+
+int main(void) {
+  printf("\"It's freezing in here,\" he said coldly.\n");
+}
