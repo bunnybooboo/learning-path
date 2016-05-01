02dd2ca19874592496fc988921abfd6cb804e421
David Ross <sw1ayfe@gmail.com>
2016-05-01 21:52:24 +0100

    adding completed exercise2

--- beginning-c/chapter-01/exercise2.c
+++ beginning-c/chapter-01/exercise2.c
@@ -1,2 +1,8 @@
 // Modify your solution for the previous exercise so that it produces all the
 // output using only one printf() statement
+#include <stdio.h>
+
+int main(void) {
+  printf("This is my name\nAnd this is my address (I'm not that stupid!)\n");
+  return 0;
+}
