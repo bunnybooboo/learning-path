e4a84fe29747ec81657266366094fffdcba012a8
David Ross <sw1ayfe@gmail.com>
2016-05-01 15:51:13 +0100

    add comments to ides

--- beginning-c/03-ides.c
+++ beginning-c/03-ides.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>                          // this is a preprocessor directive
 
-int main(void) {
-  printf("Beware the Ides of March!\n");
-  return 0;
-}
+int main(void)                              // this identifies the function main()
+{                                           // this marks the beginning of main()
+  printf("Beware the Ides of March!\n");    // this line outputs a quotation
+  return 0;                                 // this returns control to the operating system
+}                                           // this marks the end of main()
