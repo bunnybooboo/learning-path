faba7ce9442ef9b8e332f9194bc1a0f391007d03
David Ross <sw1ayfe@gmail.com>
2016-05-01 20:46:42 +0100

    file 5 was a repeat so adding 4 6 7

---
+++ beginning-c/04-command.c
@@ -0,0 +1,7 @@
+#include <stdio.h>                          // this is a preprocessor directive
+
+int main(void)                              // this identifies the function main()
+{                                           // this marks the beginning of main()
+  printf("\"Beware the Ides of March!\"\n");    // this line outputs a quotation
+  return 0;                                 // this returns control to the operating system
+}                                           // this marks the end of main()
---
+++ beginning-c/06-alert.c
@@ -0,0 +1,7 @@
+#include <stdio.h>                          // this is a preprocessor directive
+
+int main(void)                              // this identifies the function main()
+{                                           // this marks the beginning of main()
+  printf("Be careful!\n\a");    // this line outputs a quotation
+  return 0;                                 // this returns control to the operating system
+}                                           // this marks the end of main()
