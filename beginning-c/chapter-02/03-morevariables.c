9b24a17723e82f623e60f87691eb0f3853da02fc
David Ross <sw1ayfe@gmail.com>
2016-05-01 22:29:51 +0100

    adding 03-morevariables

---
+++ beginning-c/chapter-02/03-morevariables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  int brothers;   // Declare a variable called brothers
+  int brides;     // and a variable called brides
+  brothers = 7;   // Store 7 in the variable brothers
+  brides = 7;     // Store 7 in the variable brides
+  printf("%d brides for %d brothers\n", brides, brothers);  // Display some output
+  return 0;
+}
