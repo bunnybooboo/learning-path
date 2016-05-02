04cccbc5cd7a903aa23a51fc7f3444abd60f9b1c
David Ross <sw1ayfe@gmail.com>
2016-05-02 11:44:30 +0100

    add exercise-3 brief

---
+++ beginning-c/chapter-02/exercise-3.c
@@ -0,0 +1,7 @@
+/*
+You’re selling a product that’s available in two versions: type 1 is a
+standard version priced at $3.50, and type 2 is a deluxe version priced at $5.50.
+Write a program using only what you’ve learned up to now that prompts for the user to
+enter the product type and a quantity, and then calculates and outputs the price for the
+quantity entered.
+*/
