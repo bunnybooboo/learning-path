497b65f0faf1013c80e804f27c10e3370bea5a50
David Ross <sw1ayfe@gmail.com>
2016-05-02 18:41:38 +0100

    adding incomplete exercise-2

--- beginning-c/chapter-02/exercise-2.c
+++ beginning-c/chapter-02/exercise-2.c
@@ -5,5 +5,43 @@
 #include <stdio.h>
 
 int main(void) {
+  float length = 0;
+  float lengthInches = 0;
+  float width = 0;
+  float widthInches;
+  float feet = 0;
+  float inches = 0;
+  float area = 0;
+
+  const float inches_per_foot = 12;
+  const float feet_per_yard = 3;
+
+  printf("Please enter the LENGTH of the room in whole feet:\n");
+  scanf("%f", &feet);
+  printf("Please enter the LENGTH of the room in the remaining inches:\n");
+  scanf("%f", &lengthInches);
+
+  feet = inches/inches_per_foot;
+  yards = feet/feet_per_yard;
+  feet = feet % feet_per_yard;
+  inches = inches % inches_per_foot;
+
+  printf("Please enter the WIDTH of the room in whole feet:\n");
+  scanf("%f", &feet);
+  printf("Please enter the WIDTH of the room in the remaining inches:\n");
+  scanf("%f", &inches);
+
+  feet = inches/inches_per_foot;
+  yards = feet/feet_per_yard;
+  feet = feet % feet_per_yard;
+  inches = inches % inches_per_foot;
+
+  area = length * width;
+
+  printf("The calculated area is: \n%.2f square yards\n", area);
+  return 0;
+
 
 }
+
+// yes this is a work in progress!
