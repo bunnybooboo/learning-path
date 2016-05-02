26b6bf37bda62e39e99819fda3d31b976bcd48ca
David Ross <sw1ayfe@gmail.com>
2016-05-02 13:58:29 +0100

    completing exercise-1

--- beginning-c/chapter-02/exercise-1.c
+++ beginning-c/chapter-02/exercise-1.c
@@ -5,5 +5,27 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int inches = 0;
+  int feet = 0;
+  int yards = 0;
+
+  const int inches_per_foot = 12;  // number of feet in inches
+  const int feet_per_yard = 3;  // number of feet in a yard
+
+
+  // prompt the user to enter a distance in inches
+  printf("Hi! Please enter a distance in inches:\n");
+  scanf("%d", &inches);
+
+  // calculate
+  feet = inches/inches_per_foot;
+  yards = feet/feet_per_yard;
+  feet = feet % feet_per_yard;
+  inches = inches % inches_per_foot;
+
+  // output distance in yards feet and inches
+  printf("That distance converts to: %d yards, %d feet, and %d inches", yards, feet, inches);
+
+  return 0;
+
 }
