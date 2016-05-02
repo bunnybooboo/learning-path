6133a2f0a925af0ce50cb111c62394b1026ecf28
David Ross <sw1ayfe@gmail.com>
2016-05-02 11:45:48 +0100

    add exercise-4 brief

---
+++ beginning-c/chapter-02/exercise-4.c
@@ -0,0 +1,6 @@
+/*
+Write a program that prompts for the user’s weekly pay in dollars and the
+hours worked to be entered through the keyboard as floating-point values. The program
+should then calculate and output the average pay per hour in the following form:
+Your average hourly pay rate is 7 dollars and 54 cents.
+*/
