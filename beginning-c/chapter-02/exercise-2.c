7f650e3b2f4ec2b7257a6b784c4772fa75b0b6b9
David Ross <sw1ayfe@gmail.com>
2016-05-02 11:43:39 +0100

    add exercise-2 brief

---
+++ beginning-c/chapter-02/exercise-2.c
@@ -0,0 +1,3 @@
+// Write a program that prompts for input of the length and width of a room in
+// feet and inches, and then calculates and outputs the floor area in square yards with two
+// decimal places after the decimal point.
